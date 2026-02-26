3aa4df2233d3071eb2206b2a19a99ff1039a0f4f
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-02-26 20:35:04 +0530

    Add files via upload

--- Bit manipulation/BitOperation.java
+++ Bit manipulation/BitOperation.java
@@ -16,13 +16,22 @@ public class BitOperation {
             System.out.println(n|bit);
     }
 
-    public static void clearIthValue(int n,int i){
+    public static int clearIthValue(int n,int i){
         int bit=~(1<<i);
-            System.out.println(n&bit);
+            return (n&bit);
+    }
+
+    public static int updateIthBit(int n,int i,int bit){
+       n=clearIthValue(n, i);
+       int bitmask=bit<<i;
+       return n|bitmask;
+
     }
     public static void main(String args[]){
         getIthValue(7, 4);
         setIthValue(10, 02);
         clearIthValue(3, 0);
+        int n=updateIthBit(10, 2, 1);
+        System.out.println(n);
     }
 }
