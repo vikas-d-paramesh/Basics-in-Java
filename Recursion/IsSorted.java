b54ce058d52867f57494580a6dcfa48b6190360b
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-03-02 19:34:10 +0530

    Add files via upload

---
+++ Recursion/IsSorted.java
@@ -0,0 +1,19 @@
+
+public class IsSorted {
+    public static boolean isSorted(int arr[], int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+
+        return isSorted(arr,i+1);
+    }
+
+    public static void main(String args[]){
+        int arr[]={1,5,3,4,5};
+        System.out.println(isSorted(arr,0));
+    }
+    
+}
