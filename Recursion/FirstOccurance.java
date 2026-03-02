e9a419b75b2010d56464369023d2f735ce0a060a
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-03-02 19:34:31 +0530

    Add files via upload

---
+++ Recursion/FirstOccurance.java
@@ -0,0 +1,19 @@
+
+
+public class FirstOccurance {
+    public static int firstOccurance(int arr[],int key,int i){
+        if (i==arr.length-1){
+            return -1;
+        }
+        if(arr[i]==key){
+            return i;
+        }
+        return firstOccurance(arr, key, i+1);
+    }
+
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5};
+        System.out.println(firstOccurance(arr,4,0));
+    }
+    
+}
