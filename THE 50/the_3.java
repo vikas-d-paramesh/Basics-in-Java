cf193f9c824592ebefb462ccd68f70f8ad5b7040
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-03-10 20:05:29 +0530

    Add files via upload

---
+++ THE 50/the_3.java
@@ -0,0 +1,14 @@
+
+
+public class the_3 {
+    public static void main(String args[]){
+        int n=10;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+            
+        }
+        System.out.println(sum);
+    }
+    
+}
