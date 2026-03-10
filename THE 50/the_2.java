644d1386f804fcf36d72f14c2e13723d22faeed5
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-03-10 20:03:13 +0530

    Logic building

---
+++ THE 50/the_2.java
@@ -0,0 +1,11 @@
+
+
+public class the_2 {
+    public static void main(String args[]){
+        int n=10;
+        for(int i=n;i>0;i--){
+            System.out.println(i);
+        }
+    }
+    
+}
