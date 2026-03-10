4eb3ca1eec1c7c09ec29e39c06089b131dc9c481
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-03-10 20:30:25 +0530

    Add files via upload

---
+++ THE 50/the_7.java
@@ -0,0 +1,12 @@
+class the_7{
+    public static void main(String args[]){
+        int n=1020;
+        int numCount=0;
+        while(n!=0){
+            n=n/10;
+            numCount++;
+
+        }
+        System.out.println(numCount);
+    }
+}
