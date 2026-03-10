8e796908d58773cc01fc62ba8df3cc52d08baa69
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-03-10 20:07:06 +0530

    Add files via upload

---
+++ THE 50/the_4.java
@@ -0,0 +1,13 @@
+
+
+public class the_4 {
+    public static void main(String args[]){
+        int n=10;
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
