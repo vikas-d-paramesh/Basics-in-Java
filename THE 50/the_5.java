c9f7e494353c19e1393202edb3a2f6ffa170e989
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-03-10 20:15:19 +0530

    Add files via upload

---
+++ THE 50/the_5.java
@@ -0,0 +1,12 @@
+
+public class the_5 {
+    public static void main(String args[]){
+        int a=19;
+        int b=8;
+        int c=40;
+int max = a>b ? a : b;
+max = max>c ? max : c;
+System.out.println(max);
+    }
+    
+}
