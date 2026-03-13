8a95682ec712f54d04ca83e4987f243abfda4a45
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-03-13 19:08:21 +0530

    Add files via upload

---
+++ THE 50/the_27.java
@@ -0,0 +1,17 @@
+
+
+public class the_27 {
+    public static boolean isSorted(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+        public static void main(String args[]){
+        int[] arr={1,2,9,4,5,6};
+        System.out.println(isSorted(arr));
+        }
+}
