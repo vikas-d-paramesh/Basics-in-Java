ab887d5ab391a6c1497b8d9ef67b191d21e500d3
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-03-14 20:13:47 +0530

    Add files via upload

---
+++ THE 50/the_31.java
@@ -0,0 +1,16 @@
+public class the_31 {
+    public static void main(String args[]){
+        String str = "hardik";
+        int count = 0;
+
+        try{
+            while(true){
+                str.charAt(count);
+                count++;
+            }
+        }
+        catch(Exception e){
+            System.out.println(count);
+        }
+    }
+}
